2fb7ca62f1001b9e63fb1fe1dc8b1013c81262fe
Lena Mishchenkova <lena-soloveva-1998@mail.ru>
2025-01-17 14:53:52 +0300

    Start working on project vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <main-layout></main-layout> -->
+  <!-- <auth-layout></auth-layout> -->
+  <component :is="layout + '-layout'" v-if="layout"></component>
+</template>
+
+<script>
+import { useRoute } from "vue-router";
+import MainLayout from "./layout/mainLayout.vue";
+import AuthLayout from "./layout/authLayout.vue";
+import { computed } from "vue";
+export default {
+  setup() {
+    const route = useRoute();
+    console.log(route);
+    
+
+    return {
+      layout: computed(() => route.meta.layout),
+    };
+  },
+  components: { MainLayout, AuthLayout },
+};
+</script>
+
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { markRaw } from 'vue'
+import Home from "@/views/home.vue";
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: markRaw(Home),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/help',
+        name: 'Help',
+        components: () => import("@/views/help.vue"),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/auth',
+        name: 'Auth',
+        components: () => import("@/views/auth.vue"),
+        meta: {
+            layout: 'auth'
+        }
+    }
+]
+console.log(import.meta.env.VITE_BASE_URL);
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.VITE_BASE_URL),
+    routes
+})
+export default router
